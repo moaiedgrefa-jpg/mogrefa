e79488601c09a40ee44034e5be6f20edb75971a4
moaiedgrefa-jpg <moaiedgrefa-jpg@users.noreply.github.com>
2026-08-22 11:48:04 +0000

    Opt in to experimental Material3 APIs to fix Kotlin compile error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += listOf(
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+            "-opt-in=androidx.compose.material.icons.ExperimentalMaterialApi"
+        )
     }
 
     buildFeatures {
